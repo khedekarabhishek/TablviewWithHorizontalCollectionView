3305ae80ee2547485ce6e5a667378570bc48f8aa
abhishekkhedekar <khedekarabhishek@gmail.com>
2018-01-23 20:07:35 +0530

    Initial Commit

---
+++ TablviewWithHorizontalCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TablviewWithHorizontalCollectionView
+//
+//  Created by Abhishek Khedekar on 23/01/18.
+//  Copyright © 2018 Abhishek Khedekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
